e121461113b093001750365299fde97f6a539c0e
Dan Iverson <daniverson2016@gmail.com>
2021-05-24 13:47:35 -0700

    Fixed warnings (#23)
    They were being identified by the whatsin script as errors

--- src/vnmr/dps.c
+++ src/vnmr/dps.c
@@ -436,7 +436,7 @@ static  double  rlloopTime[MAX_RLLOOP+1];
 static  int     rlloopMult[MAX_RLLOOP+1];
 static  char    psgfile[MAXPATH];
 static  char    dpsfile[MAXPATH];
-static  char 	inputs[MAXPATH * 2];
+static  char 	inputs[MAXPATH * 2 + 32];
 static  char    *dps_label = NULL;
 static  char    *cmd_name = NULL;
 static  char    *argArray[64];
@@ -2451,7 +2451,7 @@ int check_args(int argc, char *argv[], int retc, char *retv[])
                return(1);
         }
         {
-            char msg[MAXPATH];
+            char msg[MAXPATH*3];
 
             sprintf(msg,"dps error: sequence '%s' does not exist", psgfile);
             if (retc == 0)
@@ -15510,8 +15510,8 @@ disp_psg_name(message)
 	int     dx;
 	char	*mess2;
 
-        if(!dpsPlot || !plotName)
-	     return;
+    if(!dpsPlot || !plotName)
+	   return;
 	dx = cal_name_width(message, 0.6);
 	if (dx > dpsW)
 	{
@@ -15543,16 +15543,16 @@ disp_psg_name(message)
 static void
 info_print(char *format, ...)
 {
-        va_list  vargs;
+    va_list  vargs;
 
-        va_start(vargs, format);
-        vsprintf(info_data, format, vargs);
-        va_end(vargs);
+    va_start(vargs, format);
+    vsprintf(inputs, format, vargs);
+    va_end(vargs);
 #ifdef VNMRJ
-	sprintf(inputs, "info add %s\n", info_data);
-	writelineToVnmrJ("dps", inputs);
+	sprintf(info_data, "info add %s\n", inputs);
+	writelineToVnmrJ("dps", info_data);
 #else 
-	put_dps_info(info_data);
+	put_dps_info(inputs);
 #endif 
 }
 
